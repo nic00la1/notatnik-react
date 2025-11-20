849f1d30702a7c374b5728c58c934258a39eb857
Nicola <nic00la.kaleta@gmail.com>
2025-11-20 18:34:42 +0100

    maly trojkat -> prawy gorny rog (dla estetyki)

--- client/notatnik/src/components/Note.jsx
+++ client/notatnik/src/components/Note.jsx
@@ -5,6 +5,7 @@ import React from 'react'
 const Note = () => {
   return (
     <div className='note-box'>
+        <div className='small-triangle'></div>
         <h2>Tytuł notatki</h2>
         <hr/>
         <p>Zawartość notatki</p>
